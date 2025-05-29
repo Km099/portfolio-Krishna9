30f280fdd6ddf35c7e55aa44bc3288d97d6a04e6
Km099 <krishnag9609@gmail.com>
2025-05-29 15:59:31 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import Header from './components/Header';
+import Intro from './components/Intro';
+import About from './components/About';
+import Work from './components/Work';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Intro />
+      <About />
+      <Work />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <nav className="nav">
+      <a href="#about">01. About</a>
+      <a href="#work">02. Work</a>
+      <a href="#contact">03. Contact</a>
+      <a href="resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a>
+    </nav>
+  );
+}
+
+export default Header;
