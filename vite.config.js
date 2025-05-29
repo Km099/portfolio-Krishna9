1245073e02d61bed23090547326a16c9c9198f2c
Km099 <krishnag9609@gmail.com>
2025-05-29 16:11:07 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio-Krishna9/'
+  base: '/portfolio-Krishna9/',
   plugins: [react()],
 })
